1a6fdf9020d7c36c9bb3659071c1a07826eb7b2e
ArtemYU <uhnovets@gmail.com>
2023-04-30 20:33:04 +0300

    some changes and add func.py

---
+++ func.py
@@ -0,0 +1,61 @@
+"""
+import sys
+sys.path.append('C:/Users/uhnov/OneDrive/Рабочий стол/УЧЕБА GB/PYTHON/Collection')
+import func as f
+"""
+
+def input_number(text) -> int:
+    print(text, end='')
+    while True:
+        try:
+            number = int(input())
+        except ValueError:
+            print('Pls input a number (int): ', end='')
+            continue
+        else:
+            break
+    return number
+
+def gener_list_rnd_randint(begin_num, end_num, len_list):
+    import random
+    return [random.randint(begin_num, end_num) for _ in range(len_list)]
+
+def gener_list_input_int_only(text):
+    length = input_number(text)
+    return [value := input_number(f'{_ + 1} value is: ') for _ in range(length)]
+
+def gener_list_rnd_randiant_unique(begin_num, end_num, len_list):
+    import random
+    list = gener_list_rnd_randint(begin_num, end_num, len_list)
+    for i in range(1, len(list)):
+        while list[i] in list[:i]:
+            list[i] = random.randint(begin_num, end_num)
+    return list
+
+def list_copy(source_list):
+    return [value for value in source_list]
+
+# функция, которая принимает два списка 
+# возвращает True, если у них есть хотя бы один общий член.
+def equal_value_list(list_1, list_2):
+    count = 0
+    for value in list_2:
+        if value in list_1:
+            count += 1
+    return True if count > 0 else False
+
+def equal_value_list_t_set(list_1, list_2):
+    return False if not set(list_1).intersection(set(list_2)) else True
+
+# функция, чтобы перемешать (в случайном порядке) и распечатать указанный список.
+def mashup_list(source_list):
+    import random
+    l_index = [i for i in range(len(source_list))]
+    l_result = []
+    rnd_num = random.randint(0, len(source_list))
+    for i in range(len(source_list)):
+        while rnd_num not in l_index:
+            rnd_num = random.randint(0, len(source_list))
+        l_result.append(source_list[rnd_num])
+        l_index.remove(rnd_num)
+    return l_result
